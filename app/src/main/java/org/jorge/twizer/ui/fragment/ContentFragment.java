111333f8f32a63cb6da303584a7f68cb83695058
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-04-29 19:29:58 +0200

    Adds dummy persistent searchbar

--- app/src/main/java/org/jorge/twizer/ui/fragment/ContentFragment.java
+++ app/src/main/java/org/jorge/twizer/ui/fragment/ContentFragment.java
@@ -6,17 +6,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.quinny898.library.persistentsearch.SearchBox;
+
 import org.jorge.twizer.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * @author stoyicker.
  */
 public class ContentFragment extends Fragment {
 
+    @InjectView(R.id.search_box)
+    SearchBox mSearchBox;
+
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              final Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, Boolean.FALSE);
+        final View view = inflater.inflate(R.layout.fragment_content, container, Boolean.FALSE);
+        ButterKnife.inject(this, view);
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mSearchBox.enableVoiceRecognition(this);
+    }
 }
