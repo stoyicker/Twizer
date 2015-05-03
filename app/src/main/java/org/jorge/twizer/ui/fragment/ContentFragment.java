300fb4e4a2870098fd3d976e8ee51c7d41d1fa4d
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-05-03 10:14:54 +0200

    Some toying around with the search box

--- app/src/main/java/org/jorge/twizer/ui/fragment/ContentFragment.java
+++ app/src/main/java/org/jorge/twizer/ui/fragment/ContentFragment.java
@@ -1,14 +1,22 @@
 package org.jorge.twizer.ui.fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
 
 import org.jorge.twizer.R;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -20,6 +28,15 @@ public class ContentFragment extends Fragment {
 
     @InjectView(R.id.search_box)
     SearchBox mSearchBox;
+
+    Context mContext;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        mContext = activity.getApplicationContext();
+    }
 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
@@ -34,6 +51,40 @@ public class ContentFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        initSearchBox();
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (isAdded() && requestCode == SearchBox.VOICE_RECOGNITION_CODE && resultCode == Activity.RESULT_OK) {
+            final ArrayList<String> matches = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            mSearchBox.populateEditText(matches);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void initSearchBox() {
+        initSearchVoiceRecognition();
+        initSearchables();
+    }
+
+    private void initSearchVoiceRecognition() {
         mSearchBox.enableVoiceRecognition(this);
+    }
+
+    private void initSearchables() {
+        //TODO Replace this by actual logic
+        final Drawable resultDrawable = mContext.getDrawable(R.drawable.ic_search_suggestion);
+        mSearchBox.addSearchable(new SearchResult("1", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("2", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("3", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("4", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("5", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("16", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("7", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("8", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("9", resultDrawable));
+        mSearchBox.addSearchable(new SearchResult("10", resultDrawable));
     }
 }
