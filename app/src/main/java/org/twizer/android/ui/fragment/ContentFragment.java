6219c9fdf2f4326d468fef48b85aac0f9d08b645
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-06-04 00:55:38 +0200

    Added rotation animation on the FAB when a new tweet is requested

--- app/src/main/java/org/twizer/android/ui/fragment/ContentFragment.java
+++ app/src/main/java/org/twizer/android/ui/fragment/ContentFragment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
 
 import com.quinny898.library.persistentsearch.SearchBox;
@@ -203,7 +205,16 @@ public final class ContentFragment extends Fragment implements NiceLoadTweetView
     }
 
     @OnClick(R.id.randomizeFab)
-    public void loadNewTweet() {
+    public void clickRandomizeFab() {
+        final RotateAnimation rotate = new RotateAnimation(180, 360, Animation.RELATIVE_TO_SELF,
+                0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(500);
+        mRandomizeFab.startAnimation(rotate);
+
+        loadNewTweet();
+    }
+
+    private void loadNewTweet() {
         final String defaultTweetId;
 
         //TODO This has to be a brand new, DIFFERENT tweet (right now it is just the last one)
